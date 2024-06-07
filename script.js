3f42a062ef7795d54b1780bf982523aa55d7cf26
Ezeorah Ikenna <ezeorahikennapaschal@gmail.com>
2024-06-07 04:11:29 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const x = Math.floor(Math.random() * 100 + 1);
+const y = Math.floor(Math.random() * 50 + 1);
+
+// To Get The Sum
+const sum = x + y;
+console.log(`${x} + ${y} = ${sum}`);
+
+// To Get The Difference
+const difference = x - y;
+console.log(`${x} - ${y} = ${difference}`);
+
+// To Get The Product
+const product = x * y;
+console.log(`${x} * ${y} = ${product}`);
+
+// To Get The Quotient
+const quotient = x / y;
+console.log(`${x} / ${y} = ${quotient}`);
+
+// To Get The Modulus
+const mod = x % y;
+console.log(`${x} % ${y} = ${mod}`);
